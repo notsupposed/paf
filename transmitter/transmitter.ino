84fb97d13db11b58e6ae24f2f779fd82d43feb31
Sergey <notsupposed@protonmail.com>
2023-03-04 17:11:29 +0300

    Remove example commands

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -86,36 +86,6 @@ const uint8_t rawDataP[]
 
 void loop()
 {
-
-#if !(defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__))
-  /*
-   * Send hand crafted data from RAM
-   * The values are NOT multiple of 50, but are taken from the NEC timing definitions
-   */
-  Serial.println(F("Send NEC 16 bit address=0xFB04 and command 0x08 with exact timing (16 bit array format)"));
-  Serial.flush();
-
-  const uint16_t rawData[] = {9000, 4500 /*Start bit*/, 560, 560, 560, 560, 560, 1690, 560,
-                              560 /*0010 0x4 of 16 bit address LSB first*/, 560, 560, 560, 560, 560, 560, 560, 560 /*0000*/, 560, 1690, 560, 1690, 560,
-                              560, 560, 1690 /*1101 0xB*/, 560, 1690, 560, 1690, 560, 1690, 560, 1690 /*1111*/, 560, 560, 560, 560, 560, 560, 560,
-                              1690 /*0001 0x08 of command LSB first*/, 560, 560, 560, 560, 560, 560, 560, 560 /*0000 0x00*/, 560, 1690, 560, 1690, 560,
-                              1690, 560, 560 /*1110 Inverted 8 of command*/, 560, 1690, 560, 1690, 560, 1690, 560, 1690 /*1111 inverted 0 of command*/,
-                              560 /*stop bit*/};                        // Using exact NEC timing
-  IrSender.sendRaw(rawData, sizeof(rawData) / sizeof(rawData[0]), KHZ); // Note the approach used to automatically calculate the size of the array.
-
-  delay(1000); // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
-#endif
-
-  /*
-   * Send byte data direct from FLASH
-   * Note the approach used to automatically calculate the size of the array.
-   */
-  Serial.println(F("Send NEC 16 bit address 0xFB0C and data 0x18 with (50 us) tick resolution timing (8 bit array format) "));
-  Serial.flush();
-  IrSender.sendRaw_P(rawDataP, sizeof(rawDataP) / sizeof(rawDataP[0]), KHZ);
-
-  delay(1000); // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
-
   Serial.println(F("Send KILL"));
 
   const uint16_t kill[] = {
@@ -139,5 +109,5 @@ void loop()
       600, 600,
       600, 600};
   IrSender.sendRaw(kill, sizeof(kill) / sizeof(kill[0]), KHZ);
-  delay(3000);
+  delay(500);
 }
